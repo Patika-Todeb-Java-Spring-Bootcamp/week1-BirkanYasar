2a00e972ceb151f02be839e5b28cb78e30f2a4c6
BirkanYasar <94866363+BirkanYasar@users.noreply.github.com>
2022-06-23 23:27:07 +0300

    First Commit

---
+++ car.java
@@ -0,0 +1,21 @@
+package IlkhaftaOdevleri;
+
+import java.awt.Color;
+import java.util.Date;
+
+public final class car {
+	
+	
+	private enum model {
+		TOYOTA,
+		FORD
+	}
+	
+	private int year;
+	private int kacKisilik;
+	private Date uretimTarihi;
+	private int km;
+	private Color renk;
+@author BirkanYasar	
+
+}
